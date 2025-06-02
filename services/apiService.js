8efa1b75702820dd8df91491025e2265fd3137f5
Ramagiri shiva shankara prasad <64155226+Shiva143-debug@users.noreply.github.com>
2025-06-02 17:23:27 +0530

    Update apiService.js

--- services/apiService.js
+++ services/apiService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // Base URL for all API calls
-const BASE_URL = 'https://exciting-spice-armadillo.glitch.me';
+// https://backend-exp-1.onrender.com
+// https://exciting-spice-armadillo.glitch.me
+const BASE_URL = 'https://backend-exp-1.onrender.com';
 
 // Create axios instance with default config
 const api = axios.create({
